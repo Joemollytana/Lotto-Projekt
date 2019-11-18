cd9b1e57e2d20e664377cdf791dbda6827ffede0
Joemollytana <andrey-shkolnik@protonmail.com>
2019-11-18 17:15:51 +0100

    OOOFFF

--- lottoscript.js
+++ lottoscript.js
@@ -2,7 +2,7 @@
 var btnContainer = document.getElementById("buttons");
 
 // Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("btn");
+var btns = btnContainer.getElementsByClassName("countryBtn");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
@@ -24,6 +24,31 @@ function showStuff(x) {
     for (i = 0; i < lottoNumbcontainer.length; i++) {
         lottoNumbcontainer[i].style.display = "none";
     }
+  // idea is to generate the buttons when needed. Maybe generate them beforehand and only show if nessesary?
+
+
+  //  if (x == de)  {
+  //    var generate = 49;
+  //  } else if (x == be) {
+  //    var generate = 45;
+  //  } else if (x == dä) {
+  //    var generate = 36;
+  //  } else if (x == us) {
+  //    var generate = 69;
+  //  } else if (x == it) {
+  //    var generate = 90;
+  //  } else {
+  //    var generate = 0;
+  //  }
+
+  //  for (i = generate; i > 0; i--){
+  //    var lottoNumb = document.createElement("BUTTON");
+  //    lottoNumb.value = i;
+  //    lottonumb.class = buttonNumbers;
+  //    lottonumb.onclick = selectNumber(i);
+  //    lottonumb.id = i
+  //    document.getElementById(x).appendChild(lottoNumb);
+  //  }
     var lottoNumb = document.getElementsByClassName("lottonumb");
     for (i = 0; i < lottoNumb.length; i++) {
         lottoNumb[i].value = "'";
